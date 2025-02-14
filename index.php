e55c16ac4c7b1a974ff7832dc46d11f373185744
shubkb07 <shub@shubkb.com>
2025-02-15 00:38:35 +0530

    Add main entry point and custom coding standards for Whizzy AI application

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This is the main entry point for the Whizzy AI application.
+ * 
+ * PHP version 8.0.0
+ * 
+ * @category AI
+ * @package  WhizzyAI
+ * @author   Shubham Kumar Bansal <shub@shubkb.com>
+ * @license  MIT License
+ * @version  1.0.0
+ * @link     https://yourwebsite.com
+ */
+
+! defined('ABSPATH') && exit;
